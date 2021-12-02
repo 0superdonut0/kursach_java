8ab2cc71ecdadb353719b7418a7018c17321181c
0superdonut0 <85933612+0superdonut0@users.noreply.github.com>
2021-12-02 21:54:55 +0300

    Add files via upload

---
+++ Sapper/src/sweeper/Box.java
@@ -0,0 +1,30 @@
+package sweeper;
+
+public enum Box { //класс-перечисление
+    ZERO,
+    NUM1,
+    NUM2,
+    NUM3,
+    NUM4,
+    NUM5,
+    NUM6,
+    NUM7,
+    NUM8,
+    BOMB,
+    OPENED,
+    CLOSED,
+    FLAGED,
+    BOMBED,
+    NOBOMB,
+    INFORM; //добавлено мной для реализации знака вопроса
+
+    public Object image;
+
+    Box getNextNumberBox() {
+        return Box.values()[this.ordinal() + 1]; //берем весь массив и возвращаем следующее значение после текущего
+    }
+
+    int getNumber() { //метод для получения номера на ячейке
+        return this.ordinal(); //используем метод ординал для получения текущего порядкового номера в перечислении
+    }
+}
